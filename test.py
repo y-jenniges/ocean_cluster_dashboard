68a4780c0ddcc3c511e551a3868490656c1a892f
yvjennig <yvonne.jenniges@awi.de>
2025-03-04 13:30:53 +0100

    make example more minimla

--- test.py
+++ test.py
@@ -3,16 +3,8 @@ from dash import dcc, html
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div([
-    html.H1("Hello, Binder!"),
-    dcc.Graph(
-        id="graph",
-        figure={
-            "data": [{"x": [1, 2, 3], "y": [10, 11, 12], "type": "line", "name": "Example"}],
-            "layout": {"title": "Simple Dash Example"}
-        }
-    )
-])
+app.layout = html.Div("Hello, Binder!")
+
 
 if __name__ == "__main__":
     app.run_server(host="0.0.0.0", port=8050, debug=True)

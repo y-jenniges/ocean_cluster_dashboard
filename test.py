12f9d1dc3101117866ce89fedff3864396a999af
yvjennig <yvonne.jenniges@awi.de>
2025-03-04 16:33:40 +0100

    trying different host

--- test.py
+++ test.py
@@ -7,4 +7,5 @@ app.layout = html.Div("Hello, Binder!")
 
 
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=8050, debug=True)
+    #app.run_server(host="0.0.0.0", port=8050, debug=True)
+    app.run_server(debug=True)

17fb95f0e1dad4bc9f94dee8c9535770a0537e83
yvjennig <yvonne.jenniges@awi.de>
2025-03-04 16:38:00 +0100

    adding a notebook containing the dashboard

--- .binder/start.sh
+++ .binder/start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-echo "Starting the Dash app..."
-python test.py --host=0.0.0.0 --port=8050
-
-
-# python dashboard_ocean_cluster_visualisation.py
+python dashboard_ocean_cluster_visualisation.ipynb

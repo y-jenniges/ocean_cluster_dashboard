262e04d5f5bfce96186a93fb27653af841b79b08
yvjennig <yvonne.jenniges@awi.de>
2025-03-06 10:56:08 +0100

    update

--- .binder/dashserverextension.py
+++ .binder/dashserverextension.py
@@ -3,6 +3,6 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """Serve the Dash app using Flask."""
-    # Popen(["python", "-m", "dashserver", "--host=0.0.0.0", "--port=5006"])
-    Popen(["dash", "serve", "dash-app","--allow-websocket-origin=*" ])
+    Popen(["python", "-m", "dashserver", "--host=0.0.0.0", "--port=5006"])
+    # Popen(["dash", "serve", "dash-app", "--allow-websocket-origin=*" ])
     # Popen(["bokeh", "serve", "bokeh-app", "--allow-websocket-origin=*"])
